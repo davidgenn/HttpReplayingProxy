dc00be217c75d803dda4b9032f0dabac2fc0c438
David Genn <davidgenn@gmail.com>
2014-06-30 22:04:12 +0100

    Added acceptance tests

--- src/main/java/davidgenn/httpreplayingproxy/proxy/ReplayingProxyHandler.java
+++ src/main/java/davidgenn/httpreplayingproxy/proxy/ReplayingProxyHandler.java
@@ -1,6 +1,7 @@
 package davidgenn.httpreplayingproxy.proxy;
 
 import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -15,19 +16,23 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import redis.clients.jedis.Jedis;
 
 
 public class ReplayingProxyHandler  extends AbstractHandler {
 
+	private static final Logger LOG = LoggerFactory.getLogger(ReplayingProxyHandler.class);
+
 	private final HttpReplayingProxyConfiguration configuration;
-	private final Jedis jedis;
+	//private final Jedis jedis;
+	private final ConcurrentHashMap<String, CachedResponse> cache = new ConcurrentHashMap<String, CachedResponse>();
 
 	public ReplayingProxyHandler(HttpReplayingProxyConfiguration configuration) {
 		this.configuration = configuration;
-		jedis = new Jedis("pub-redis-19186.us-east-1-3.4.ec2.garantiadata.com", 19186);
-		jedis.auth("proxycache");
+		//		jedis = new Jedis("pub-redis-19186.us-east-1-3.4.ec2.garantiadata.com", 19186);
+		//		jedis.auth("proxycache");
 	}
 
 	@Override
@@ -36,12 +41,14 @@ public class ReplayingProxyHandler  extends AbstractHandler {
 	throws IOException, ServletException {
 
 		RequestToProxy requestToProxy = RequestToProxy.from(baseRequest);
-		String cachedContent = jedis.get(requestToProxy.toString());
+		LOG.info("Proxying="+requestToProxy.toString());
+		CachedResponse cachedContent = cache.get(requestToProxy.toString());
 		if (cachedContent == null) {
+			LOG.info("Cache-MISS="+requestToProxy.toString());
 			CloseableHttpClient httpclient = HttpClients.createDefault();
-			
+
 			HttpRequestBase httpRequest = null;
-			
+
 			switch (requestToProxy.getHttpMethod()) {
 			case GET:
 				httpRequest = new HttpGet(configuration.getUrlToProxyTo() + requestToProxy.getRequestPath());
@@ -52,19 +59,22 @@ public class ReplayingProxyHandler  extends AbstractHandler {
 			default:
 				break;
 			}
-			
+
 			httpRequest.setHeaders(requestToProxy.getHeaders());
-			
+
 			CloseableHttpResponse proxiedResponse = httpclient.execute(httpRequest);
 			String content = IOUtils.toString(proxiedResponse.getEntity().getContent());
 			response.setStatus(proxiedResponse.getStatusLine().getStatusCode());
 			baseRequest.setHandled(true);
 			response.getWriter().write(content);
-			jedis.set(requestToProxy.toString(), content);
-			jedis.expire(requestToProxy.toString(), 5);
+			cache.put(requestToProxy.toString(), new CachedResponse(proxiedResponse.getStatusLine().getStatusCode(), content));
+			//			jedis.expire(requestToProxy.toString(), 5);
 		} else {
-			response.getWriter().write(cachedContent);
+			LOG.info("Cache-HIT="+requestToProxy.toString());
+			response.addHeader("x-http-replaying-proxy-cached", "true");
+			response.getWriter().write(cachedContent.getResponse());
+			response.setStatus(cachedContent.getStatusCode());
+			baseRequest.setHandled(true);
 		}
-
 	}
 }
--- src/main/java/davidgenn/httpreplayingproxy/proxy/RequestToProxy.java
+++ src/main/java/davidgenn/httpreplayingproxy/proxy/RequestToProxy.java
@@ -1,14 +1,25 @@
 package davidgenn.httpreplayingproxy.proxy;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Enumeration;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.input.CloseShieldInputStream;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
+import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.entity.InputStreamEntity;
 import org.apache.http.message.BasicHeader;
 import org.eclipse.jetty.server.Request;
+
+import com.google.common.collect.Lists;
 
 public class RequestToProxy {
 
@@ -16,7 +27,7 @@ public class RequestToProxy {
 	private final String requestPath;
 	private final HttpMethod method;
 	private final HttpEntity body;
-	
+
 	public RequestToProxy(Header[] headers, String requestPath, HttpMethod httpMethod, HttpEntity body) {
 		this.headers = headers;
 		this.body = body;
@@ -26,17 +37,28 @@ public class RequestToProxy {
 
 	public static RequestToProxy from(Request baseRequest) throws IOException {
 		Set<Header> headers = new HashSet<Header>();
-		while(baseRequest.getHeaderNames().hasMoreElements()) {
-			String headerName = baseRequest.getHeaderNames().nextElement();
+		Enumeration<String> headerNames = baseRequest.getHeaderNames();
+		while(headerNames.hasMoreElements()) {
+			String headerName = headerNames.nextElement();
+			if ("Content-Length".equals(headerName)) {
+				continue;
+			}
 			BasicHeader header = new BasicHeader(headerName, baseRequest.getHeader(headerName));
 			headers.add(header);
 		}
-		
-		InputStreamEntity body = new InputStreamEntity(baseRequest.getInputStream()); 
-		
+
+		ByteArrayEntity body = new ByteArrayEntity(IOUtils.toByteArray(baseRequest.getInputStream())); 
+		baseRequest.extractParameters();
+		String queryString = baseRequest.getQueryString();
+		String path = null;
+		if (queryString == null) {
+			path = baseRequest.getPathInfo();	
+		} else {
+			path = baseRequest.getPathInfo() + "?"+ queryString;	
+		}
 		return new RequestToProxy(
-				(Header[]) headers.toArray(), 
-				baseRequest.getPathInfo(), 
+				headers.toArray(new Header[0]), 
+				path, 
 				HttpMethod.valueOf(baseRequest.getMethod()), 
 				body);
 	}
@@ -57,5 +79,37 @@ public class RequestToProxy {
 
 	public HttpEntity getBody() {
 		return body;
+	}
+
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("requestPath=" + requestPath);
+		sb.append("method=" + method);
+		sb.append("headers=" + headersToString());
+		try {
+			sb.append("body=" + new String(IOUtils.toByteArray(body.getContent()))); 
+		} catch (IllegalStateException e) {
+			throw new RuntimeException(e);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} 
+		return sb.toString();
+	}
+	
+	private String headersToString() {
+		StringBuffer sb = new StringBuffer();
+		List<Header> headerList = Lists.newArrayList(headers);
+		Collections.sort(headerList, new Comparator<Header>() {
+
+			@Override
+			public int compare(Header o1, Header o2) {
+				return o1.getName().compareTo(o2.getName());
+			}
+
+		});
+		for (Header header: headerList) {
+			sb.append(header.getName() + "=" + header.getValue());
+		}
+		return sb.toString();
 	}
 }
